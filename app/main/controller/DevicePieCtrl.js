648bf71f5aa26579bcf4c916f68d0976ba739a36
lrcUnlimited <lirunchao@126.com>
2016-07-12 14:50:24 +0800

    Merge branch 'master' of https://github.com/lrcUnlimited/monitor-frontend
    # Conflicts:
    #	app/main/controller/DevicePieCtrl.js

--- app/main/controller/DevicePieCtrl.js
+++ app/main/controller/DevicePieCtrl.js
@@ -7,13 +7,15 @@ devicePieModule.controller("DevicePieCtrl", function ($scope, $http, $rootScope,
     var type = $cookieStore.get("USER_TYPE");
     onDevice = [];
     offDevice = [];
+    arrearagePercentageArray = [];
     if (accountId) {
         function showPieChartOne() {
             $('#pieContainerOne').highcharts({
                 chart: {
                     plotBackgroundColor: null,
                     plotBorderWidth: null,
-                    plotShadow: false
+                    plotShadow: false,
+                    type: 'pie'
                 },
                 title: {
                     text: '在线（离线）百分比'
@@ -26,14 +28,17 @@ devicePieModule.controller("DevicePieCtrl", function ($scope, $http, $rootScope,
                         allowPointSelect: true,
                         cursor: 'pointer',
                         dataLabels: {
-                            enabled: false
-                        },
-                        showInLegend: true
+                            enabled: true,
+                            format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+                            style: {
+                                color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+                            }
+                        }
                     }
                 },
                 series: [{
-                    type: 'pie',
-                    name: 'Browser share',
+                    name: 'Brands',
+                    colorByPoint: true,
                     data: [
                         onDevice,
                         offDevice
@@ -42,111 +47,72 @@ devicePieModule.controller("DevicePieCtrl", function ($scope, $http, $rootScope,
             });
         }
 
-    function showPieChart() {
-        $(function () {
-            $('#pieContainerOne').highcharts({
-                chart: {
-                    plotBackgroundColor: null,
-                    plotBorderWidth: null,
-                    plotShadow: false,
-                    type: 'pie'
-                },
-                title: {
-                    text: '在线（离线）百分比'
-                },
-                tooltip: {
-                    pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
-                },
-                plotOptions: {
-                    pie: {
-                        allowPointSelect: true,
-                        cursor: 'pointer',
-                        dataLabels: {
-                            enabled: true,
-                            format: '<b>{point.name}</b>: {point.percentage:.1f} %',
-                            style: {
-                                color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
-                            }
-                        }
-                    }
-                },
-                series: [{
-                    name: 'Brands',
-                    colorByPoint: true,
-                    data: [{
-                        name: '在线',
-                        y: 55.0
-                    }, {
-                        name: '离线',
-                        y: 45.0,
-                        sliced: true,
-                        selected: true
-                    }]
-                }]
-            });
-        });
-        $(function () {
-            $('#pieContainerTwo').highcharts({
-                chart: {
-                    plotBackgroundColor: null,
-                    plotBorderWidth: null,
-                    plotShadow: false,
-                    type: 'pie'
-                },
-                title: {
-                    text: '欠费率'
-                },
-                tooltip: {
-                    pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
-                },
-                plotOptions: {
-                    pie: {
-                        allowPointSelect: true,
-                        cursor: 'pointer',
-                        dataLabels: {
-                            enabled: true,
-                            format: '<b>{point.name}</b>: {point.percentage:.1f} %',
-                            style: {
-                                color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
-                            }
-                        }
-                    }
-                },
-                series: [{
-                    name: 'Brands',
-                    colorByPoint: true,
-                    data: [{
-                        name: 'Firefox',
-                        y: 56.33
-                    }, {
-                        name: 'Chrome',
-                        y: 24.03,
-                        sliced: true,
-                        selected: true
-                    }, {
-                        name: 'IE',
-                        y: 10.38
-                    }, {
-                        name: 'Safari',
-                        y: 4.77
-                    }, {
-                        name: 'Opera',
-                        y: 0.91
-                    }, {
-                        name: 'others',
-                        y: 0.2
-                    }]
-                }]
-            });
-        });
+        function showPieChart() {
 
-    }
-        function showPieChartTwo(){
+            $(function () {
+                $('#pieContainerTwo').highcharts({
+                    chart: {
+                        plotBackgroundColor: null,
+                        plotBorderWidth: null,
+                        plotShadow: false,
+                        type: 'pie'
+                    },
+                    title: {
+                        text: '欠费率'
+                    },
+                    tooltip: {
+                        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+                    },
+                    plotOptions: {
+                        pie: {
+                            allowPointSelect: true,
+                            cursor: 'pointer',
+                            dataLabels: {
+                                enabled: true,
+                                format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+                                style: {
+                                    color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+                                }
+                            }
+                        }
+                    },
+                    series: [{
+                        name: 'Brands',
+                        colorByPoint: true,
+                        data: [{
+                            name: 'Firefox',
+                            y: 56.33
+                        }, {
+                            name: 'Chrome',
+                            y: 24.03,
+                            sliced: true,
+                            selected: true
+                        }, {
+                            name: 'IE',
+                            y: 10.38
+                        }, {
+                            name: 'Safari',
+                            y: 4.77
+                        }, {
+                            name: 'Opera',
+                            y: 0.91
+                        }, {
+                            name: 'others',
+                            y: 0.2
+                        }]
+                    }]
+                });
+            });
+
+        }
+
+        function showPieChartTwo() {
             $('#pieContainerTwo').highcharts({
                 chart: {
                     plotBackgroundColor: null,
                     plotBorderWidth: null,
-                    plotShadow: false
+                    plotShadow: false,
+                    type: 'pie'
                 },
                 title: {
                     text: '欠费率'
@@ -159,46 +125,55 @@ devicePieModule.controller("DevicePieCtrl", function ($scope, $http, $rootScope,
                         allowPointSelect: true,
                         cursor: 'pointer',
                         dataLabels: {
-                            enabled: false
-                        },
-                        showInLegend: true
+                            enabled: true,
+                            format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+                            style: {
+                                color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+                            }
+                        }
                     }
                 },
                 series: [{
                     type: 'pie',
                     name: 'Browser share',
-                    data: [
-                        ['Firefox', 45.0],
-                        ['IE', 26.8],
-                        {
-                            name: 'Chrome',
-                            y: 12.8,
-                            sliced: true,
-                            selected: true
-                        },
-                        ['Safari', 8.5],
-                        ['Opera', 6.2],
-                        ['Others', 0.7]
-                    ]
+                    data: arrearagePercentageArray,
                 }]
             });
         }
 
-        function requestTotalNumOfDeviceStatus(){
-            $http.get(HTTP_BASE + 'device/e_queryTotalNumOfDeviceStatus?accountId=' + accountId + '&type=' +　type)
+        function requestTotalNumOfDeviceStatus() {
+            $http.get(HTTP_BASE + 'device/e_queryTotalNumOfDeviceStatus?accountId=' + accountId + '&type=' + type)
                 .success(function (data) {
-                    onDevice = ['在线', data[0]];
-                    offDevice = ['离线', data[1]];
-
+                    onDevice = ['在线', data[0][0] / (data[0][0] + data[1][0])];
+                    offDevice = ['离线', data[1][0] / (data[0][0] + data[1][0])];
                     showPieChartOne();
                 });
         }
 
-        function requestLesseeDeviceInfo(){
-            $http.get(HTTP_BASE + 'device/e_queryLesseeDeviceInformation?accountId=' + accountId + '&type=' +　type)
+        function requestArrearagePercentage(){
+            $http.get(HTTP_BASE + 'device/e_queryArrearagePercentage?accountId=' + accountId + '&type=' + type)
                 .success(function (data) {
-                    for(i = 0; i < data.length; i++)
-                    {
+                    for(i = 0; i < data.length; i++){
+                        arrearagePercentageArray.push([data[i].lessee, data[i].percantage]);
+                    }
+                    showPieChartTwo();
+                });
 
-    showPieChart();
+        }
+
+
+        function requestLesseeDeviceInfo() {
+            $http.get(HTTP_BASE + 'device/e_queryLesseeDeviceInformation?accountId=' + accountId + '&type=' + type)
+                .success(function (data) {
+                    $scope.lesseeDeviceInfoList = data;
+                });
+        }
+
+        requestTotalNumOfDeviceStatus();
+        requestArrearagePercentage();
+        requestLesseeDeviceInfo();
+
+    }
 });
+
+
