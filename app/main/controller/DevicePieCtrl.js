1e3d71840b5d012b937f7dde100098198be77c51
lrcUnlimited <lirunchao@126.com>
2016-07-12 21:52:42 +0800

    Merge branch 'master' of https://github.com/lrcUnlimited/monitor-frontend
    # Conflicts:
    #	app/main/controller/DevicePieCtrl.js

--- app/main/controller/DevicePieCtrl.js
+++ app/main/controller/DevicePieCtrl.js
@@ -210,72 +210,38 @@ devicePieModule.controller("DevicePieCtrl", function ($scope, $http, $rootScope,
                 "display" : "none"
             })
         }
-        //function showPieChartTwo() {
-        //    $('#pieContainerTwo').highcharts({
-        //        chart: {
-        //            plotBackgroundColor: null,
-        //            plotBorderWidth: null,
-        //            plotShadow: false,
-        //            type: 'pie'
-        //        },
-        //        title: {
-        //            text: '欠费率'
-        //        },
-        //        tooltip: {
-        //            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
-        //        },
-        //        plotOptions: {
-        //            pie: {
-        //                allowPointSelect: true,
-        //                cursor: 'pointer',
-        //                dataLabels: {
-        //                    enabled: true,
-        //                    format: '<b>{point.name}</b>: {point.percentage:.1f} %',
-        //                    style: {
-        //                        color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
-        //                    }
-        //                }
-        //            }
-        //        },
-        //        series: [{
-        //            type: 'pie',
-        //            name: 'Browser share',
-        //            data: arrearagePercentageArray,
-        //        }]
-        //    });
+
+        //function requestTotalNumOfDeviceStatus() {
+        //    $http.get(HTTP_BASE + 'device/e_queryTotalNumOfDeviceStatus?accountId=' + accountId + '&type=' + type)
+        //        .success(function (data) {
+        //            onDevice = ['在线', data[0][0] / (data[0][0] + data[1][0])];
+        //            offDevice = ['离线', data[1][0] / (data[0][0] + data[1][0])];
+        //            showPieChartOne();
+        //        });
         //}
 
-        function requestTotalNumOfDeviceStatus() {
-            $http.get(HTTP_BASE + 'device/e_queryTotalNumOfDeviceStatus?accountId=' + accountId + '&type=' + type)
-                .success(function (data) {
-                    onDevice = ['在线', data[0][0] / (data[0][0] + data[1][0])];
-                    offDevice = ['离线', data[1][0] / (data[0][0] + data[1][0])];
-                    showPieChartOne();
-                });
-        }
-
-        function requestArrearagePercentage(){
-            $http.get(HTTP_BASE + 'device/e_queryArrearagePercentage?accountId=' + accountId + '&type=' + type)
-                .success(function (data) {
-                    for(i = 0; i < data.length; i++){
-                        arrearagePercentageArray.push([data[i].lessee, data[i].percantage]);
-                    }
-                    showPieChartTwo();
-                });
-
-        }
+        //function requestArrearagePercentage(){
+        //    $http.get(HTTP_BASE + 'device/e_queryArrearagePercentage?accountId=' + accountId + '&type=' + type)
+        //        .success(function (data) {
+        //            for(i = 0; i < data.length; i++){
+        //                arrearagePercentageArray.push([data[i].lessee, data[i].percantage]);
+        //            }
+        //            showPieChartTwo();
+        //        });
+        //
+        //}
 
 
-        function requestLesseeDeviceInfo() {
-            $http.get(HTTP_BASE + 'device/e_queryLesseeDeviceInformation?accountId=' + accountId + '&type=' + type)
-                .success(function (data) {
-                    $scope.lesseeDeviceInfoList = data;
-                });
-        }
+        //function requestLesseeDeviceInfo() {
+        //    $http.get(HTTP_BASE + 'device/e_queryLesseeDeviceInformation?accountId=' + accountId + '&type=' + type)
+        //        .success(function (data) {
+        //            $scope.lesseeDeviceInfoList = data;
+        //        });
+        //}
 
-        requestTotalNumOfDeviceStatus();
-        requestArrearagePercentage();
-        requestLesseeDeviceInfo();
+        //requestTotalNumOfDeviceStatus();
+        //requestArrearagePercentage();
+        //requestLesseeDeviceInfo();
 
     }
 
