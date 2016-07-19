e687ed3fe457b82d9631779a2b8ff600768b520a
Ljx** <729869585@qq.com>
2016-07-19 16:09:53 +0800

    TOP10柱状图修改

--- app/main/controller/DevicePieCtrl.js
+++ app/main/controller/DevicePieCtrl.js
@@ -227,7 +227,7 @@ devicePieModule.controller("DevicePieCtrl", function ($scope, $http, $rootScope,
                             enabled: false
                         },
                         title: {
-                            text: '最差租赁商TOP10'
+                            text: '优质租赁商TOP10'
                         },
                         exporting: {
                             enabled:false
@@ -301,7 +301,7 @@ devicePieModule.controller("DevicePieCtrl", function ($scope, $http, $rootScope,
                             enabled:false
                         },
                         title: {
-                            text: '优质租赁商TOP10'
+                            text: '最差租赁商TOP10'
                         },
 
                         xAxis: {
